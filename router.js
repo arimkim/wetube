68b5a2ca8d2b089c92c0a85fe00dd789a4331ec1
arimkim <arim721@gmail.com>
2020-02-24 22:10:28 +0900

    controller of MVC

--- router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-export const userRouter = express.Router();
-
-userRouter.get('/', (req, res) => res.end('user index'));
-userRouter.get('/edit', (req, res) => res.end('user edit'));
-userRouter.get('/password', (req, res) => res.end('user password'));
