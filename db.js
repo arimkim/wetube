084827013fc57f6b9b90c24ba900c7626cd8aca4
arimkim <arim721@gmail.com>
2020-02-29 00:56:46 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 123456,
+    title: 'Video Awesome',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'arim',
+      email: 'arim@arim.com'
+    }
+  },
+  {
+    id: 654322,
+    title: 'Video Super',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'arim',
+      email: 'arim@arim.com'
+    }
+  },
+  {
+    id: 7865345,
+    title: 'Video Perfect',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'arim',
+      email: 'arim@arim.com'
+    }
+  },
+  {
+    id: 582392,
+    title: 'Video Great',
+    description: 'This is something I love',
+    views: 24,
+    videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    creator: {
+      id: 121212,
+      name: 'arim',
+      email: 'arim@arim.com'
+    }
+  }
+];
